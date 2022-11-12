36b8d6fbb46d1c84f17d7a3d113de27b723c9a53
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2022-11-13 01:09:03 +0300

    adjusted

--- src/components/countries/Country.tsx
+++ src/components/countries/Country.tsx
@@ -1,41 +1,16 @@
-import React,{useEffect,useState, ReactComponentElement,useRef,useCallback} from 'react'
-import { NavLink, Navigate} from 'react-router-dom'
-import CountryPage from './../pages/CountryPage'
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { NavLink} from 'react-router-dom'
+
 
 
 const Country : React.FC <{ countryName: string, countryFlag:string, countryRegion:string, population:number; capital:string; textColorForCountry:string ; countryBackgroundColor?:string ;subRegion?:string}> = (props) => {
-    const [isSending, setIsSending] = useState(false);
-    const isMounted = useRef(true)
-
-    useEffect(() => {
-        return () => {
-          isMounted.current = false
-        }
-      }, [])
-    
-  const sendRequest = useCallback(async () => {
-    // don't send again while we are sending
-    if (isSending) return
-    // update state
-    setIsSending(true)
-    // send the actual request
-    Navigate(`country/${props.countryName}` as never)
-    // once the request is sent, update state again
-    if (isMounted.current) // only update if we are still mounted
-      setIsSending(false)
-  }, [isSending])
-   
-  
-    
  
-   
-
+  
     return (
        
-        
-         
-            <div onClick={sendRequest} className={`w-[264px] h-[336px] mx-auto my-8 text-left rounded-b-md drop-shadow-md`}>
+        <NavLink to={`/country/${props.countryName}`}>
+
+            <div className={`w-[264px] h-[336px] mx-auto my-8 text-left rounded-b-md drop-shadow-md`}>
                 <img className={'rounded-t-md h-[160px] w-[264px]'}  src={props.countryFlag} alt=""/>
                <div className={`px-4 ${props.countryBackgroundColor} rounded-b-md p-2`}>
                 <h1 className={`font-bold text-left ${props.textColorForCountry}`}>{props.countryName}</h1>
@@ -45,8 +20,8 @@ const Country : React.FC <{ countryName: string, countryFlag:string, countryRegi
                  </div>
             
             </div>
-          
-      
+           
+        </NavLink> 
     )
 }
 
